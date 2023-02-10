982b50ba8dc8acd2de7278effc3ded6b5257a30f
Constantin <konstantindanilov19@hotmail.com>
2023-02-10 10:49:55 +0200

    added mongoose and defined the schema and the model for my blog data base

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 
 const app = express()
 
-app.set('view engine', 'ejs')
+const mongoDB = 'mongodb+srv://BlogUser:deefterAdi2022@blog.w1zgknw.mongodb.net/?retryWrites=true&w=majority'
+mongoose.set('strictQuery', true);
+mongoose.connect(mongoDB).then((result) => app.listen(3000)).catch((error) => console.log(error))
 
-app.listen(3000)
+app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   const blogs = [
-    {title: 'The title of the blog', snippet: 'The description ot the text of the blog call it whatever u want'},
-    {title: 'The title of the blog', snippet: 'The description ot the text of the blog call it whatever u want'},
-    {title: 'The title of the blog', snippet: 'The description ot the text of the blog call it whatever u want'},
+    { title: 'The title of the blog', snippet: 'The description ot the text of the blog call it whatever u want' },
+    { title: 'The title of the blog', snippet: 'The description ot the text of the blog call it whatever u want' },
+    { title: 'The title of the blog', snippet: 'The description ot the text of the blog call it whatever u want' },
   ]
-  res.render('index', {title: 'Home', blogs})
+  res.render('index', { title: 'Home', blogs })
 })
 
 app.get('/about', (req, res) => {
-  res.render('about', {title: 'About'})
+  res.render('about', { title: 'About' })
 })
 
 app.get('/blogs/create', (req, res) => {
-  res.render('create', {title: 'Create'})
+  res.render('create', { title: 'Create' })
 })
 
 app.use((req, res) => {
-  res.status(404).render('404', {title: 'Error'})
+  res.status(404).render('404', { title: 'Error' })
 })
